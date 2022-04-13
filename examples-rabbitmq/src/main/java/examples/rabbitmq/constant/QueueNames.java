df42d1f9f14a965038b28ef90b68a51c2eb85887
应卓 <yingzhor@gmail.com>
2022-04-14 06:52:50 +0800

    2022-04-14 06:52:48

--- examples-rabbitmq/src/main/java/examples/rabbitmq/constant/QueueNames.java
+++ examples-rabbitmq/src/main/java/examples/rabbitmq/constant/QueueNames.java
@@ -6,8 +6,10 @@ public final class QueueNames {
         super();
     }
 
-    public static final String QUEUE_X = "queue.x";
+    public static final String PREFIX_MIRROR2 = "mirror2.";
 
-    public static final String QUEUE_X_DLQ = "queue.x.dlq";
+    public static final String QUEUE_X = PREFIX_MIRROR2 + "queue.x";
+
+    public static final String QUEUE_X_DLQ = PREFIX_MIRROR2 + "queue.x.dlq";
 
 }
