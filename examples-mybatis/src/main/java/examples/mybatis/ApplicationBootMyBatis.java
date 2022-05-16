818150c44e37c43d39e29d9aef97ae32756fc13b
应卓 <yingzhor@gmail.com>
2022-05-16 10:12:28 +0800

    2022-05-16 10:12:26

--- examples-mybatis/src/main/java/examples/mybatis/ApplicationBootMyBatis.java
+++ examples-mybatis/src/main/java/examples/mybatis/ApplicationBootMyBatis.java
@@ -1,9 +1,24 @@
 package examples.mybatis;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @MapperScan("examples.mybatis.mapper")
+@EnableTransactionManagement
 class ApplicationBootMyBatis {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
+    }
+
 }
