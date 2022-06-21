10a951130c845413064c58fcbc678b868d7435c1
应卓 <yingzhor@gmail.com>
2022-06-21 17:53:22 +0800

    2022-06-21 17:53:21

--- examples-jps/src/main/java/examples/jpa/mapper/UserMapper.java
+++ examples-jps/src/main/java/examples/jpa/mapper/UserMapper.java
@@ -2,6 +2,10 @@ package examples.jpa.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import examples.jpa.domain.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper extends BaseMapper<User> {
+
+    public User haha(@Param("aaa") String aaa);
+
 }
